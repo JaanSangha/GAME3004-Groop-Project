015598b10cfce510dafb2507e01021834ed5a056
abdulhakeemidr <56049093+abdulhakeemidr@users.noreply.github.com>
2022-02-23 22:54:33 -0500

    update vanishing platform

--- Assets/Scripts/Platforms/VanishingPlatform.cs
+++ Assets/Scripts/Platforms/VanishingPlatform.cs
@@ -11,26 +11,45 @@ public class VanishingPlatform : PlatformController
     public float IntangibilityAlpha = 0.5f;
     private MeshRenderer meshRender;
     private Material floorMat;
+    private Material[] materials;
     private Collider platformCollider;
 
     void Start()
     {
-        floor = transform.GetChild(0).gameObject;
-        meshRender = floor.GetComponent<MeshRenderer>();
-        floorMat = meshRender.material;
-        originalColor = floorMat.color;
+        // floor = transform.GetChild(0).gameObject;
+        // meshRender = floor.GetComponent<MeshRenderer>();
+        // floorMat = meshRender.material;
+        // originalColor = floorMat.color;
 
-        // Switches the material (instance) render mode to transparent
-        floorMat.SetOverrideTag("RenderType", "Transparent");
-        floorMat.SetInt("_SrcBlend", (int)UnityEngine.Rendering.BlendMode.SrcAlpha);
-        floorMat.SetInt("_DstBlend", (int)UnityEngine.Rendering.BlendMode.OneMinusSrcAlpha);
-        floorMat.SetInt("_ZWrite", 0);
-        floorMat.DisableKeyword("_ALPHATEST_ON");
-        floorMat.EnableKeyword("_ALPHABLEND_ON");
-        floorMat.DisableKeyword("_ALPHAPREMULTIPLY_ON");
-        floorMat.renderQueue = 3000;
+        // floorMat.SetOverrideTag("RenderType", "Transparent");
+        // floorMat.SetInt("_SrcBlend", (int)UnityEngine.Rendering.BlendMode.SrcAlpha);
+        // floorMat.SetInt("_DstBlend", (int)UnityEngine.Rendering.BlendMode.OneMinusSrcAlpha);
+        // floorMat.SetInt("_ZWrite", 0);
+        // floorMat.DisableKeyword("_ALPHATEST_ON");
+        // floorMat.EnableKeyword("_ALPHABLEND_ON");
+        // floorMat.DisableKeyword("_ALPHAPREMULTIPLY_ON");
+        // floorMat.renderQueue = 3000;
 
+        materials = new Material[transform.childCount];
 
+        for(int i = 0; i < transform.childCount; i++)
+        {
+            floor = transform.GetChild(i).gameObject;
+            meshRender = floor.GetComponent<MeshRenderer>();
+            materials[i] = meshRender.material;
+            originalColor = materials[i].color;
+            
+            // Switches the material (instance) render mode to transparent
+            materials[i].SetOverrideTag("RenderType", "Transparent");
+            materials[i].SetInt("_SrcBlend", (int)UnityEngine.Rendering.BlendMode.SrcAlpha);
+            materials[i].SetInt("_DstBlend", (int)UnityEngine.Rendering.BlendMode.OneMinusSrcAlpha);
+            materials[i].SetInt("_ZWrite", 0);
+            materials[i].DisableKeyword("_ALPHATEST_ON");
+            materials[i].EnableKeyword("_ALPHABLEND_ON");
+            materials[i].DisableKeyword("_ALPHAPREMULTIPLY_ON");
+            materials[i].renderQueue = 3000;
+        }
+        
         finalAlpha = Mathf.Clamp01(finalAlpha);
         //floorMat.color = new Color(originalColor.r, originalColor.g, originalColor.b, finalAlpha);
 
@@ -48,16 +67,30 @@ public class VanishingPlatform : PlatformController
         Color finalColor = new Color(originalColor.r, originalColor.g, originalColor.b, finalAlpha);
         elapsedTime += Time.deltaTime;
         float percentComplete = elapsedTime / desiredTime;
-        floorMat.color = Color.Lerp(originalColor, finalColor, speedCurve.Evaluate(Mathf.PingPong(percentComplete, 1)));
         
-        if(floorMat.color.a < IntangibilityAlpha)
+        //floorMat.color = Color.Lerp(originalColor, finalColor, speedCurve.Evaluate(Mathf.PingPong(percentComplete, 1)));
+        // if(floorMat.color.a < IntangibilityAlpha)
+        // {
+        //     platformCollider.enabled = false;
+        // }
+        // else if(floorMat.color.a >= IntangibilityAlpha)
+        // {
+        //     platformCollider.enabled = true;
+        // }
+        
+        for(int i = 0; i < materials.Length; i++)
         {
-            platformCollider.enabled = false;
+            materials[i].color = Color.Lerp(originalColor, finalColor, speedCurve.Evaluate(Mathf.PingPong(percentComplete, 1)));
+            if(materials[i].color.a < IntangibilityAlpha)
+            {
+                platformCollider.enabled = false;
+            }
+            else if(materials[i].color.a >= IntangibilityAlpha)
+            {
+                platformCollider.enabled = true;
+            }
         }
-        else if(floorMat.color.a >= IntangibilityAlpha)
-        {
-            platformCollider.enabled = true;
-        }
+        
     }
 
     private void OnCollisionEnter(Collision other) 
