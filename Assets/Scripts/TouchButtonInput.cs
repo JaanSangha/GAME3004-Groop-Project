932682d80f272d702b0bb5a4467606e67fddb51f
abdulhakeemidr <56049093+abdulhakeemidr@users.noreply.github.com>
2022-03-12 00:34:22 -0500

    Organized Scripts Folder

---
+++ Assets/Scripts/TouchButtonInput.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TouchButtonInput : MonoBehaviour
+{
+    [SerializeField]
+    GameObject Abutton;
+    [SerializeField]
+    GameObject Ybutton;
+    [SerializeField]
+    GameObject Xbutton;
+    [SerializeField]
+    GameObject Bbutton;
+
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
